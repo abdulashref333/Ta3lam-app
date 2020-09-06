319e14c29490f21d804ac9c411d8a483216b4edc
Ahmed Nabil <ahmednabl992@eng.aswu.edu.eg>
2020-09-06 19:04:22 +0200

    Add sign up file

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+const EMAIL = document.querySelector("#exampleInputEmail1");
+const PASSWORD = document.querySelector("#exampleInputPassword1");
+const LOGINBUTTON = document.querySelector('#Login-button');
+const rightPassword = "123456";
+const rightEmail = "ahmed@gg.com";
+console.log(EMAIL);
+
+
+// EMAIL.addEventListener("keyup",function(){
+//     // checkEmail(EMAIL.value);
+// })
+// PASSWORD.addEventListener("keyup",function(){
+//     console.log(PASSWORD.value);
+// })
+
+function checkEmail (){
+if(EMAIL.value ==rightEmail) return true;
+else return;
+}
+function checkPassword() {
+    if (PASSWORD.value == rightPassword) return true ;
+    else return;
+}
+
+LOGINBUTTON.addEventListener("click",function(){
+    if(checkEmail() && checkPassword()) {
+        alert("This is right");
+    }
+    else {
+        alert("This is wrong")
+    }
+})
