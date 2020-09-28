a09ae4c35801ecbd33e9ed24f6438388f2c814c6
Ahmed Nabil <ahmednabl992@eng.aswu.edu.eg>
2020-09-28 18:15:26 +0200

    Add student page

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,26 @@
+
 const EMAIL = document.querySelector("#exampleInputEmail1");
 const PASSWORD = document.querySelector("#exampleInputPassword1");
 const LOGINBUTTON = document.querySelector('#Login-button');
 const rightPassword = "123456";
 const rightEmail = "ahmed@gg.com";
-console.log(EMAIL);
+// console.log(EMAIL);
+const myForm = document.querySelector("#myloginform");
+myForm.addEventListener("submit",function(e){
+    e.preventDefault();
 
+    const formData = new FormData(this);
+    fetch("http://localhost:3000/api/users/login",{
+        method: "post",
+        body: formData
+    }).then(function(response) {
+        return response.text();
+    }).then(function(text){
+        console.log(text);
+    }).catch(function(error){
+        console.error();
+    })
+})
 
 // EMAIL.addEventListener("keyup",function(){
 //     // checkEmail(EMAIL.value);
@@ -13,20 +29,20 @@ console.log(EMAIL);
 //     console.log(PASSWORD.value);
 // })
 
-function checkEmail (){
-if(EMAIL.value ==rightEmail) return true;
-else return;
-}
-function checkPassword() {
-    if (PASSWORD.value == rightPassword) return true ;
-    else return;
-}
+// function checkEmail (){
+// if(EMAIL.value ==rightEmail) return true;
+// else return;
+// }
+// function checkPassword() {
+//     if (PASSWORD.value == rightPassword) return true ;
+//     else return;
+// }
 
-LOGINBUTTON.addEventListener("click",function(){
-    if(checkEmail() && checkPassword()) {
-        alert("This is right");
-    }
-    else {
-        alert("This is wrong")
-    }
-})
+// LOGINBUTTON.addEventListener("click",function(){
+//     if(checkEmail() && checkPassword()) {
+//         alert("This is right");
+//     }
+//     else {
+//         alert("This is wrong")
+//     }
+// })
